ab231b22ccf3177c4aecbb5baf4dcdb2a506c7a3
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-04-08 22:16:44 +0600

    prepare for heroku deployment

--- config.py
+++ config.py
@@ -4,5 +4,4 @@ import os
 class Config:
     PROPAGATE_EXCEPTIONS = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or "sqlite:///database.db"
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///database.db"

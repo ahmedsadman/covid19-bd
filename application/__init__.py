76dbcd6751b285109377a271b5c9ed62982114f0
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-04-09 17:49:00 +0600

    added cors

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 # globally accessible variables
 db = SQLAlchemy()
+cors = CORS()
 
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
+
+    cors.init_app(app)
     db.init_app(app)
 
     with app.app_context():
